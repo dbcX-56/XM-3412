ddadbb4d336601eeeca7d86d1a1e538fd16c57e0
dbcX-56 <vuongtrankimphung@gmail.com>
2026-04-11 11:36:58 +0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,6 +1,8 @@
 (function() {
   const pageName = window.location.pathname.split("/").pop().replace(".html","");
 
+  console.log("PAGE:", pageName); // 👈 thêm dòng này
+
   fetch("https://script.google.com/macros/s/AKfycbxyvafuSgZQv4Ah-DsC9Du3uvM6g1nc9qtonwM15MhWpxGpSLN7AcibsOG55ON_G6GUNw/exec", {
     method: "POST",
     mode: "no-cors",
